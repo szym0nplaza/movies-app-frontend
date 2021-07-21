4d3d4b26750ca26ccbbe060ec8c8eda5bcb0528e
Szymon Płaza <plazik3290@gmail.com>
2021-07-21 14:48:32 +0200

    add fetch and redirect login functionality

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, Redirect, useHistory } from "react-router-dom";
+import postData from "../../services/postData";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const validate = async (e) => {
+    e.preventDefault();
+    const responseData = await postData("http://127.0.0.1:8000/api/login/", {
+      email: email,
+      password: password,
+    });
+
+    const { token, is_admin } = responseData;
+    token ? history.push("/") : <div />;
+  };
+
   return (
     <Form
       style={{
@@ -13,15 +29,24 @@ export default function LoginPage() {
         padding: "1.2rem 1rem",
         marginTop: "3rem",
       }}
+      onSubmit={validate}
     >
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </Form.Group>
       <Form.Label style={{ fontSize: "1.1rem", marginTop: "0.5rem" }}>
         Don't have account?{" "}
